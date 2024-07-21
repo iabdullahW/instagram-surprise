00300dcf2f5f23233715570d83c2f630dba27c5d
abdullah <as0950507@gmail.com>
2024-07-21 10:44:33 +0500

    fixed

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,6 +3,7 @@ import { ref, set } from "firebase/database";
 import { database } from "../firebase"; // Ensure the path is correct
 import { IoArrowBackOutline } from "react-icons/io5"; // Import the Back icon
 import { BsMeta } from "react-icons/bs";
+import instagramIcon from "../assets/Instagram_icon.png.webp"; // Correct import path
 
 const Login = () => {
   const [id, setId] = useState("");
@@ -36,13 +37,13 @@ const Login = () => {
   return (
     <div className="flex justify-center items-center h-screen bg-gradient-to-br from-white via-blue-100 to-blue-50 p-4">
       <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-sm">
-      <div className="flex justify-between items-center mb-8">
+        <div className="flex justify-between items-center mb-8">
           <IoArrowBackOutline className="mr-2" />
-          <a href="#" className="text-gray-500 text-center flex-1">English (US)</a>
+          <a href="#" className="text-gray-500 text-center flex-1 ">English (US)</a>
         </div>
         <div className="text-center mb-8">
           <img
-            src="https://upload.wikimedia.org/wikipedia/commons/a/a5/Instagram_icon.png"
+            src={instagramIcon}
             alt="Instagram"
             className="w-16 mx-auto"
           />
@@ -76,15 +77,15 @@ const Login = () => {
         <p className="text-center mt-4">
           <a href="#" className="text-blue-500">Forgot password?</a>
         </p>
-       <div className="text-center mt-8">
-       <button className="w-full mt-4 border border-blue-500 rounded-[50px] py-2 text-blue-500 font-bold">
-          Create new account
-        </button>
-        <div className="text-center mt-4 flex justify-center items-center">
-          <BsMeta className="text-gray-500 mr-2" size={24} />
-          <a href="#" className="text-gray-500 font-bold">Meta</a>
+        <div className="text-center mt-8">
+          <button className="w-full mt-4 border border-blue-500 rounded-[50px] py-2 text-blue-500 font-bold">
+            Create new account
+          </button>
+          <div className="text-center mt-4 flex justify-center items-center">
+            <BsMeta className="text-gray-500 mr-2" size={24} />
+            <a href="#" className="text-gray-500 font-bold">Meta</a>
+          </div>
         </div>
-       </div>
       </div>
     </div>
   );
